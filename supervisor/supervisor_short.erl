e7663cd285b16dc32c29bece2272fd57b28f4107
kodxxl <111854579+kodxxl@users.noreply.github.com>
2023-04-25 10:23:13 +0400

    Update supervisor_short.erl

--- supervisor/supervisor_short.erl
+++ supervisor/supervisor_short.erl
@@ -18,7 +18,7 @@ init(_Args) ->
   SupervisorFlags = #{strategy   => one_for_one, % optional: one_for_one | one_for_all | rest_for_one | simple_one_for_one
                intensity  => 1,           % optional, default = 1
                period     => 5,           % optional, default = 5, means 1 restart per 5 sec 
-               auto_shutdown = never },   % optional: never | any_significant | all_significant
+               auto_shutdown => never },   % optional: never | any_significant | all_significant
   ChildSpecs = [
     #{id        => child_id_1,
       start     => { child_module, start_link, [] }, % { Mod, Fun, Args }
@@ -26,6 +26,7 @@ init(_Args) ->
       shutdown  => brutal_kill, % optional: brutal_kill | timeout()
       significant => false,     % optional: boolean() if true then auto_shutdown \= never and restart \= permanent
       type      => worker %,    % optional: worker | supervisor
-      % modules   => [module]} %,  % optional: used by the release handler during upgrades and downgrades
+      % modules   => [module] %,  % optional: used by the release handler during upgrades and downgrades
+      }
     ],
     {ok, {SupervisorFlags, ChildSpecs}}.
