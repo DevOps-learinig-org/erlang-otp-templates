5dded7003dca3dc506a3c1a06d871bac84edbba5
kodxxl <111854579+kodxxl@users.noreply.github.com>
2023-04-25 16:31:00 +0400

    Update gen_server_short.erl

--- gen_server/gen_server_short.erl
+++ gen_server/gen_server_short.erl
@@ -16,16 +16,35 @@ start_link(RegisterName) ->
   gen_server:start_link(
     RegisterName, ?MODULE, ?INITARGS, ?STARTOPTS
   ).
+% {ok, Pid :: pid()} | ignore | {error, Reason :: term()}
 
 init(InitArgs) ->
   State = InitArgs,
   {ok, State}.
+% {ok,State} | {ok,State,_} | {ok,_,Timeout} | {ok,_,hibernate} | {ok,_,{continue,Continue}}
+% {stop,Reason}
+% ignore
 
 handle_call(_Request, _From, State) ->
   Reply = ok,
   NewState = State,
   {reply, Reply, NewState}.
+% {reply,Reply, NewState}
+%  | {reply,Reply,NewState,Timeout}
+%  | {reply,Reply,NewState,hibernate}
+%  | {reply,Reply,NewState,{continue,Continue}}
+%  | {noreply,NewState}
+%  | {noreply,NewState,Timeout}
+%  | {noreply,NewState,hibernate}
+%  | {noreply,NewState,{continue,Continue}}
+%  | {stop,Reason,Reply,NewState}
+%  | {stop,Reason,NewState}
 
 handle_cast(_Request, State) ->
   NewState = State,
   {noreply, NewState}.
+% {noreply,NewState}
+%  | {noreply,NewState,Timeout}
+%  | {noreply,NewState,hibernate}
+%  | {noreply,NewState,{continue,Continue}}
+%  | {stop,Reason,NewState}
