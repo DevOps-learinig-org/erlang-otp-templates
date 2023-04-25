53f450d9c0a6b471c6651af8bbaeaed93bba8753
kodxxl <111854579+kodxxl@users.noreply.github.com>
2023-04-25 08:10:36 +0400

    Update supervisor_short.erl

---
+++ supervisor/supervisor_short.erl
@@ -0,0 +1,29 @@
+%---MODULE-NAME---(change this comment together with -module().)
+-module(module_name).
+%---GEN_SERVER---
+-behavour(supervisor).
+
+-export([start_link/0, start_link/1]).
+-export([init/1]).
+
+-define(INITARGS, []).
+
+start_link() ->
+  supervisor:start_link(?MODULE, ?INITARGS).
+
+start_link(SupervisorName) ->
+  supervisor:start_link(SupervisorName, ?MODULE, ?INITARGS).
+
+init(_Args) ->
+  SupFlags = #{strategy   => one_for_one,
+               intensity  => 1,
+               period     => 5 },
+  ChildSpecs = [
+    #{id        => ch3,
+      start     => {ch3, start_link, []},
+      restart   => permanent,
+      shutdown  => brutal_kill,
+      type      => worker,
+      modules   => [ch3]} %,
+    ],
+    {ok, {SupFlags, ChildSpecs}}.
